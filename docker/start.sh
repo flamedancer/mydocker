3a0ccc822d07af11abea761fbc224a227e4da616
flame <flame@flamedeMacBook-Pro.local>
2018-03-22 09:38:22 +0800

    up

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,8 @@ docker run \
     -d \
     --name some-nginx \
     --network host \
-    -v "${HOME}":/root \
+    -v "${HOME}":/data \
     -v "${HOME}/nginx_docker/config/nginx.conf":/etc/nginx/nginx.conf \
     -v "${HOME}/nginx_docker/config/conf.d":/etc/nginx/conf.d \
+    -v "${HOME}/nginx_docker/config/logs":/var/log/nginx \
     nginx:alpine
